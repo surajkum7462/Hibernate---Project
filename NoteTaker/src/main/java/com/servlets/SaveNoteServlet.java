4792115ffd25a6fc991b9f05c4c6f18a1dd0730d
surajkum7462 <162699747+surajkum7462@users.noreply.github.com>
2024-09-29 22:21:37 +0530

    Add files via upload

---
+++ NoteTaker/src/main/java/com/servlets/SaveNoteServlet.java
@@ -0,0 +1,72 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import com.entities.Note;
+import com.helper.FactoryProvider;
+
+
+public class SaveNoteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+    public SaveNoteServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String title=request.getParameter("title");
+			String content=request.getParameter("content");
+			Note note=new Note(title,content,new Date());
+//			System.out.println(note.getId()+":"+note.getTitle());
+			//hibernate :save()
+			
+			Session s=FactoryProvider.getFactory().openSession();
+			Transaction tx=s.beginTransaction();
+			
+			s.save(note);
+			tx.commit();
+			
+			
+			response.setContentType("text/html");
+			PrintWriter out=response.getWriter();
+			out.println("<h1 style='text-align:center;'>Note is added successfully</h1>");
+			out.println("<h1 style='text-align:center;'><a href='all_notes.jsp'>View all notes</a></h1>");
+			
+			
+			
+			
+			
+			s.close();
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}catch(Exception e)
+		
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ NoteTaker/src/main/webapp/UpdateNote.jsp
@@ -0,0 +1,64 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@page import="com.helper.*,org.hibernate.*,com.entities.*" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<%@include file="all_js_css.jsp"%>
+</head>
+<body>
+
+	<div class="container">
+		<%@include file="navbar.jsp"%>
+
+		<h1>Edit Your Note</h1>
+		<br>
+		<%
+		
+		int noteId=Integer.parseInt(request.getParameter("note_id").trim());
+		Session s=FactoryProvider.getFactory().openSession();
+		
+		
+		
+		Note note=(Note)s.get(Note.class, noteId);
+		
+		%>
+		<form action="UpdateServlet" method="post">
+		<input value="<%= note.getId()%>" name="noteId" type="hidden"/>
+			<div class="form-group">
+				<label for="title">Note title</label> 
+				<input 
+				name="title"
+				   required
+					type="text" 
+					class="form-control" 
+					id="title"
+					aria-describedby="emailHelp" 
+					placeholder="Enter Here"
+					value="<%= note.getTitle()%>"/> 
+					
+			</div>
+			<div class="form-group">
+				<label for="content">Note Content</label> 
+				<textarea 
+				name="content"
+				required
+				id="content" 
+				placeholder="Enter your content"
+				class="form-control"
+				style="height:300px"; >
+				<%= note.getContent() %>
+				</textarea>
+			</div>
+			<div class="container text-center">
+			  <button type="submit" class="btn btn-success purple">Save your note</button>
+			
+			
+			</div>
+			
+		</form>
+		
+</body>
+</html>
